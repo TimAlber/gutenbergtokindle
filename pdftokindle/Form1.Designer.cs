e1a0ffc8855346f12a8263e7a1e5e0cf0161dcd6
TimAlber <t.midaidu@gmail.com>
2015-12-27 20:36:45 +0100

    allofit

---
+++ pdftokindle/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace pdftokindle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(867, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Senden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(867, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(357, 436);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listbox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(376, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(485, 134);
+            this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listbox2_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(377, 154);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(484, 318);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(295, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 482);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Buch zu Kindle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ pdftokindle/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.IO.Compression;
+
+namespace pdftokindle
+{
+    public partial class Form1 : Form
+    {
+        List<string> url = new List<string>();
+        List<string> urls = new List<string>();
+        List<string> paths = new List<string>();
+        List<string> files = new List<string>();
+        string next = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (string i in paths)
+            {
+                try
+                {
+                    SmtpClient mailServer = new SmtpClient("smtp.gmail.com", 587);
+                    mailServer.EnableSsl = true;
+                    mailServer.Credentials = new System.Net.NetworkCredential("someone@gmail.com", "something");
+
+                    string from = "timjonathanalber@gmail.com";
+                    //string to = "gaby.alber_18@kindle.com";
+                    string to = "t.planet@web.de";
+                    MailMessage msg = new MailMessage(from, to);
+                    msg.Subject = "";
+                    msg.Body = "";
+                    msg.Attachments.Add(new Attachment(i));
+                    mailServer.Send(msg);
+                    //textBox1.Text = textBox1.Text + " Done";
+                }
+                catch (Exception ex)
+                {
+                    //textBox1.Text = ex.ToString();
+                    Console.WriteLine("Unable to send email. Error : " + ex);
+                }
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string curent = wc.DownloadString("http://www.gutenberg.org/robot/harvest?filetypes[]=html&langs[]=de");
+            Console.WriteLine("dwnled");
+            next = Regex.Match(curent, @"(<a.*?>Next Page</a>)").Groups[1].Value;
+            Regex ofset = new Regex(@"[^\d]");
+            next =  ofset.Replace(next, "");
+            Console.WriteLine("next: "+next);
+            foreach (LinkItem i in LinkFinder.Find(curent))
+            {
+               string name = loadbookstolist(i);
+               listBox1.Items.Add(name);
+            }
+            listBox1.SelectionMode = SelectionMode.One;
+            listBox2.SelectionMode = SelectionMode.One;
+            
+        }
+        public string loadbookstolist(LinkItem i)
+        {
+            WebClient wc = new WebClient();
+            string num = Regex.Split(Regex.Split(i.ToString(), " ")[0], " /n ")[0];
+            Console.WriteLine(num);
+            //string[] all = Regex.Split(num, "/");
+            Console.WriteLine("all: " + Regex.Split(Regex.Split(Regex.Split(i.ToString(), " ")[0], " /n ")[0], "/")[7]);
+            string all = Regex.Split(Regex.Split(Regex.Split(i.ToString(), " ")[0], " /n ")[0], "/")[7];
+            url.Add(num);
+            Console.WriteLine("http://www.gutenberg.org/ebooks/search/?query=%" + all);
+            string name = wc.DownloadString("http://www.gutenberg.org/ebooks/search/?query=%" + all);
+            name = Regex.Match(name, @"<span class=""title"">.*?</span>").Groups[1].Value;
+            Console.WriteLine("name : "+ name);
+            return name;
+            //listBox1.Items.Add(name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string i in urls)
+            {
+                WebClient wc = new WebClient();
+                string p = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName(), ".zip");
+                Console.WriteLine(p);
+                wc.DownloadFile(i,p);
+                string p2 = Path.Combine(Path.GetTempPath(), "/extracted/", Path.GetRandomFileName(), ".html");
+                System.IO.Compression.ZipFile.ExtractToDirectory(p,p2);
+                paths.Add(p2);
+                Console.WriteLine(p2);
+            }
+        }
+
+        private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Regex.Split(i.ToString(), "] [")[0]
+            urls.Add(url[listBox1.SelectedIndex]);
+            listBox2.Items.Add(url[listBox1.SelectedIndex] + Environment.NewLine);
+        }
+
+        private void listbox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string newurl = urls[listBox2.SelectedIndex].ToString() + ".html.images";
+            newurl = Regex.Replace(newurl, "//", "http://");
+            Console.WriteLine("list click: " + newurl);
+            webBrowser1.Navigate(new System.Uri(newurl));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string curent = wc.DownloadString("http://www.gutenberg.org/robot/harvest?offset="+next+"&filetypes[]=html&langs[]=de");
+            Console.WriteLine("dwnled");
+            next = Regex.Match(curent, @"(<a.*?>Next Page</a>)").Groups[1].Value;
+            Regex ofset = new Regex(@"[^\d]");
+            next = ofset.Replace(next, "");
+            Console.WriteLine("next: " + next);
+            foreach (LinkItem i in LinkFinder.Find(curent))
+            {
+                loadbookstolist(i);
+            }
+            listBox1.SelectionMode = SelectionMode.One;
+            listBox2.SelectionMode = SelectionMode.One;
+        }
+    }
+    public struct LinkItem
+    {
+        public string Href;
+        public string Text;
+
+        public override string ToString()
+        {
+            return Href + " \n " + Text;
+        }
+    }
+
+    static class LinkFinder
+    {
+        public static List<LinkItem> Find(string file)
+        {
+            List<LinkItem> list = new List<LinkItem>();
+
+            // 1.
+            // Find all matches in file.
+            MatchCollection m1 = Regex.Matches(file, @"(<a.*?>http://www.gutenberg.lib.md.us/.*?</a>)",
+                RegexOptions.Singleline);
+
+            // 2.
+            // Loop over each match.
+            foreach (Match m in m1)
+            {
+                string value = m.Groups[1].Value;
+                LinkItem i = new LinkItem();
+
+                // 3.
+                // Get href attribute.
+                Match m2 = Regex.Match(value, @"href=\""(.*?)\""",
+                RegexOptions.Singleline);
+                if (m2.Success)
+                {
+                    i.Href = m2.Groups[1].Value;
+                }
+
+                // 4.
+                // Remove inner tags from text.
+                string t = Regex.Replace(value, @"\s*<.*?>\s*", "",
+                RegexOptions.Singleline);
+                i.Text = t;
+
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
